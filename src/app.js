98e35165c07861bdd637277e84a4d55a5fe78cb7
samnewga <newgate90s@gmail.com>
2020-04-08 18:03:20 -0700

    603

--- src/app.js
+++ src/app.js
@@ -110,3 +110,32 @@ app.post('/list', (req, res) => {
 });
 
 app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  // set up winston
+  const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [new winston.transports.File({ filename: 'info.log' })],
+  });
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
